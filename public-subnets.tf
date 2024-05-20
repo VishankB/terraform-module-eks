61a8e9f01c5ee42321eb1190ae9fb37d88bdb41e
VishankB <156158620+VishankB@users.noreply.github.com>
2024-05-20 22:07:45 +0530

    Create public-subnets.tf
    Configuration to create public subnets for VPC in different AZ zones

---
+++ public-subnets.tf
@@ -0,0 +1,28 @@
+resource "aws_subnet" "public-us-east-1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.vpc_cidr
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "public-us-east-1a"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+
+resource "aws_subnet" "public-us-east-1b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.vpc_cidr
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "public-us-east-1b"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+
+
+### need to update config for AZ zones
