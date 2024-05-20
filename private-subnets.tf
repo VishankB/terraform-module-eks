cc7627982237a1d4b5088a40bf14039a75548269
VishankB <156158620+VishankB@users.noreply.github.com>
2024-05-20 22:05:28 +0530

    Create private-subnets.tf
    Configuration to create private subnets for VPC in different AZ zones

---
+++ private-subnets.tf
@@ -0,0 +1,26 @@
+resource "aws_subnet" "private-us-east-1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.vpc_cidr
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name"                            = "private-us-east-1a"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+
+resource "aws_subnet" "private-us-east-1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.vpc_cidr
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name"                            = "private-us-east-1b"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+
+
+### need to update config for availability zones.
