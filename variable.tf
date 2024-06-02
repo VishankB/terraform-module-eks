30de0388952a1369fe277b4f8be88f93f8d618e3
VishankB <156158620+VishankB@users.noreply.github.com>
2024-06-02 21:40:38 +0530

    Update variable.tf
    added new variables for public and private subnets

--- variable.tf
+++ variable.tf
@@ -1,13 +1,15 @@
 variable "kubernetes_version" {
-  default     = 1.27    ## need to check
   description = "kubernetes version"
 }
-
 variable "vpc_cidr" {
-  default     = "need to decide"
   description = "default CIDR range of the VPC"
 }
+variable "private_subnet_cidr" {
+  description = "private subnet CIDR range of the VPC"
+}
+variable "public_subnet_cidr" {
+  description = "public subnet CIDR range of the VPC"
+}
 variable "aws_region" {
-  default = "Need to give as a input"
   description = "aws region"
 }
